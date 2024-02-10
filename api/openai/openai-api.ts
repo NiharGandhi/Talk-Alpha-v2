faff7bfb29065a5588290f92b1e15ee4954307dc
Nihar Gandhi <nihargandhi93@gmail.com>
2024-02-10 21:45:53 +0400

    detled

--- api/openai/openai-api.ts
+++
@@ -1,8 +0,0 @@
-import { OpenAI } from 'openai';
-
-export class OpenAIAPI {
-    private client: OpenAI;
-
-    constructor() {
-        this.client = new OpenAI(process.env.NEXT_PUBLIC_OPENAI_API_KEY);
-    }}
--- api/openai/openai.ts
+++
@@ -1,7 +0,0 @@
-import { OpenAIAPI } from './openai-api';
-
-const openai = new OpenAIAPI();
-
-export async function createAssistant() {
-  return openai.createAssistant();
-}
